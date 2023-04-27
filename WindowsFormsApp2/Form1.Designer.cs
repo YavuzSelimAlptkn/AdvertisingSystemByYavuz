88114105cfd858902ca866090d04a764944f2c5a
YavuzSelim <yavuzselimalptkn@gmail.com>
2023-04-27 15:07:56 +0300

    My first project.

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -29,36 +29,66 @@ namespace WindowsFormsApp2
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.Main = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Advertising = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Main)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Advertising)).BeginInit();
             this.SuspendLayout();
             // 
-            // axWindowsMediaPlayer1
+            // Main
             // 
-            this.axWindowsMediaPlayer1.Enabled = true;
-            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(-1, 0);
-            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
-            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
-            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(454, 244);
-            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.Main.Enabled = true;
+            this.Main.Location = new System.Drawing.Point(-1, 0);
+            this.Main.Name = "Main";
+            this.Main.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Main.OcxState")));
+            this.Main.Size = new System.Drawing.Size(1920, 1080);
+            this.Main.TabIndex = 0;
+            this.Main.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.Main_PlayStateChange);
+            // 
+            // Advertising
+            // 
+            this.Advertising.Enabled = true;
+            this.Advertising.Location = new System.Drawing.Point(-8, -20);
+            this.Advertising.Name = "Advertising";
+            this.Advertising.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Advertising.OcxState")));
+            this.Advertising.Size = new System.Drawing.Size(1920, 1080);
+            this.Advertising.TabIndex = 1;
+            this.Advertising.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.Advertising_PlayStateChange);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.Advertising);
+            this.Controls.Add(this.Main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = new System.Drawing.Point(1366, 0);
             this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Main)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Advertising)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private AxWMPLib.AxWindowsMediaPlayer Main;
+        private AxWMPLib.AxWindowsMediaPlayer Advertising;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormsApp2
 {
@@ -15,6 +16,113 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+        }
+        private Random sayi = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string FolderPath = Path.Combine(exePath, "Videos");
+            string[] files = Directory.GetFiles(FolderPath);
+            int sayi2;
+            do
+            {
+                sayi2 = sayi.Next(1, files.Length);
+            } while ("Video" + sayi2 + ".mp4" == Main.URL);
+            string videoPath = Path.Combine(FolderPath, "Video" + sayi2 + ".mp4");
+            Main.URL = videoPath;
+            Main.BringToFront();
+            Main.stretchToFit = true;
+            Main.Ctlcontrols.play();
+            timer1.Interval = 10000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            if (Main.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string FolderPath = Path.Combine(exePath, "Ads");
+                string[] files = Directory.GetFiles(FolderPath);
+                Random sayi = new Random();
+                int sayi2 = sayi.Next(1, files.Length);
+                Main.Ctlcontrols.pause();
+                string videoPath = Path.Combine(FolderPath, "AD" + sayi2 + ".mp4");
+                Advertising.URL = videoPath;
+                Advertising.Ctlcontrols.play();
+                Advertising.BringToFront();
+                Advertising.stretchToFit = true;
+            }
+            else
+            {
+                string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string FolderPath = Path.Combine(exePath, "Videos");
+                string[] files = Directory.GetFiles(FolderPath);
+                int sayi2;
+                do
+                {
+                    sayi2 = sayi.Next(1, files.Length);
+                } while ("Video" + sayi2 + ".mp4" == Main.URL);
+                string videoPath = Path.Combine(FolderPath, "Video" + sayi2 + ".mp4");
+                Main.URL = videoPath;
+                Main.BringToFront();
+                Main.stretchToFit = true;
+                Main.Ctlcontrols.play();
+            }
+            timer1.Interval = 5 * 60 * 1000;
+            timer1.Start();
+        }
+
+        private void Main_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (Main.playState == WMPLib.WMPPlayState.wmppsMediaEnded)
+            {
+                Main.Ctlcontrols.pause();
+                string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string FolderPath = Path.Combine(exePath, "Videos");
+                string[] files = Directory.GetFiles(FolderPath);
+                int sayi2;
+                do
+                {
+                    sayi2 = sayi.Next(1, files.Length);
+                } while ("Video" + sayi2 + ".mp4" == Main.URL);
+                string videoPath = Path.Combine(FolderPath, "Video" + sayi2 + ".mp4");
+                Main.URL = videoPath;
+                Main.BringToFront();
+                Main.stretchToFit = true;
+                Main.Ctlcontrols.play();
+            }
+        }
+
+        private void Advertising_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            if (Advertising.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                if(Main.playState == WMPLib.WMPPlayState.wmppsStopped)
+                {
+                    string exePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                    string FolderPath = Path.Combine(exePath, "Videos");
+                    string[] files = Directory.GetFiles(FolderPath);
+                    int sayi2;
+                    do
+                    {
+                        sayi2 = sayi.Next(1, files.Length);
+                    } while ("Video" + sayi2 + ".mp4" == Main.URL);
+                    string videoPath = Path.Combine(FolderPath, "Video" + sayi2 + ".mp4");
+                    Main.URL = videoPath;
+                    Main.BringToFront();
+                    Main.stretchToFit = true;
+                    Main.Ctlcontrols.play();
+                }
+                else
+                {
+                    Main.Ctlcontrols.play();
+                    Main.BringToFront();
+                    Main.stretchToFit = true;
+                    timer1.Start();
+                }
+            }
         }
     }
 }
